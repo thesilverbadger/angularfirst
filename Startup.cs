6308729c688d3434dd6020393cc9645bb29aee84
Tom Wire <tom.w.wire@gmail.com>
2016-12-24 15:18:58 +0000

    Initial

---
+++ Startup.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace AngularFirst
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
