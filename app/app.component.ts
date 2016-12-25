c45605de4d329d895a7e9e7a2732b3304ed31e39
Tom Wire <tom.w.wire@gmail.com>
2016-12-25 22:25:00 +0000

    Angular bindings

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',    
+    template: `
+        <input type="text" [value]="food" (input)="foodInput($event)" />
+        <p>Sriracha sauce is great with {{ food }}</p>
+    `
+})
+export class AppComponent { 
+    food = 'kielbasa';
+
+    foodInput(event: Event) {
+        const target = event.target as HTMLInputElement;
+        this.food = target.value;
+    }
+}
